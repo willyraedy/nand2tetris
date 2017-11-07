c71f04135c98eb09dc9e86d01bc3c07861ff8383
Willy Raedy <wraedy@gmail.com>
2017-11-07 00:54:10 -0600

    completes ch 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,68 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Screen starts at RAM[16384]
+// ends at RAM[24575]
+// keyboard stored at RAM[24576]
+
+// initiate position at first pixel
+  @16384
+  D=A
+  @0
+  M=D
+// check keyboard
+  @CHECK
+  0;JMP
+// have a blackening loop
+(FILL)
+  // skip logic if screen is already full
+  @0
+  D=M
+  @24575
+  D=D-A
+  @CHECK
+  D;JGT
+  // if not, fill next spot
+  @0
+  D=M
+  A=D
+  M=-1
+  @0
+  M=M+1
+  // and then check keyboard
+  @CHECK
+  0;JMP
+// have a clearning loop
+(CLEAR)
+  // skip logic if screen is already empty
+  @0
+  D=M
+  @16384
+  D=D-A
+  @CHECK
+  D;JLT
+  // if not, clear previous spot
+  @0
+  D=M
+  A=D
+  M=0
+  @0
+  M=M-1
+  // and then check keyboard
+  @CHECK
+  0;JMP
+// jumps to one or the other depending on keyboard input
+(CHECK)
+  @24576
+  D=M
+  @FILL
+  D;JNE
+  @CLEAR
+  D;JEQ
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// add R0 to R2, R1 times
+  @i
+  M=0
+  @2
+  M=0
+(LOOP)
+  // if counter = R1, jump to END
+  @i
+  D=M
+  @1
+  D=D-M
+  @END
+  D;JGE
+  // add R0 to R2
+  @0
+  D=M
+  @2
+  M=M+D
+  // increment counter
+  @i
+  M=M+1
+  // and continue the loop
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
+
+
+
+
